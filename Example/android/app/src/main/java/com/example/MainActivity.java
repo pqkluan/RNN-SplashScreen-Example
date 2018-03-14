f28fb90630bca278c96ca84a1b9cc0a02515f729
Luân Phạm <luan.pham@piksal.io>
2018-03-14 09:14:12 +0700

    Implement android splash screen

--- Example/android/app/src/main/java/com/example/MainActivity.java
+++ Example/android/app/src/main/java/com/example/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.widget.LinearLayout;
+
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout splash = new LinearLayout(this);
+        Drawable launch_screen_bitmap = ContextCompat.getDrawable(getApplicationContext(),R.drawable.launch_screen_bitmap);
+        splash.setBackground(launch_screen_bitmap);
 
+        return splash;
+    }
 }
