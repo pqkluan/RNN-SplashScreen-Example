1eb63041d20630212c32137adf56273821947022
Luân Phạm <luan.pham@piksal.io>
2018-03-14 11:48:31 +0700

    Add bonus step and orignal asset

--- Example/App.js
+++ Example/App.js
@@ -5,14 +5,21 @@ const WelcomeScreenID = "pqkluan.Welcome";
 Navigation.registerComponent(WelcomeScreenID, () => WelcomeScreen);
 
 export const startApp = () => {
+  let j = 0;
+  for (let i = 0; i < 909000000; i++) {
+    j += i;
+  }
+  console.log(j)
   // start the app
   Navigation.startSingleScreenApp({
     screen: {
       screen: WelcomeScreenID,
       title: "Welcome",
       navigatorStyle: {}
-    }
+    },
+    animationType: "fade"
   });
 };
+// setTimeout(() => startApp(), 1000);
 
 startApp();
