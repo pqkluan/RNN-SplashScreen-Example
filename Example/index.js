8cd4a3dadad2f38747b15e2e1f0b79c6f59c0082
Luân Phạm <luan.pham@piksal.io>
2018-04-11 17:28:20 +0700

    Cleanup code and background

--- Example/index.js
+++ Example/index.js
@@ -1,4 +1,19 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { Navigation } from "react-native-navigation";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
 
-AppRegistry.registerComponent('Example', () => App);
+const WelcomeScreenID = "pqkluan.Welcome";
+Navigation.registerComponent(WelcomeScreenID, () => WelcomeScreen);
+
+export const startApp = () => {
+  // start the app
+  Navigation.startSingleScreenApp({
+    screen: {
+      screen: WelcomeScreenID,
+      title: "Welcome",
+      navigatorStyle: { screenBackgroundColor: "white" }
+    },
+    animationType: "fade"
+  });
+};
+
+startApp();
